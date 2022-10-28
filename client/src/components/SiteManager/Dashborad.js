d017a5797c26377ed0748b82b639121fbf3d2fdd
chethuuu <IT20065294@my.sliit.lk>
2022-10-28 22:12:53 +0530

    Modify Pages

--- client/src/components/SiteManager/Dashborad.js
+++ client/src/components/SiteManager/Dashborad.js
@@ -1,77 +1,61 @@
-  
-import { Link, useNavigate, useParams } from "react-router-dom";
- import React from 'react'
-import avatar from "../images/order.gif"
-import avatar2 from "../images/order3.gif"
-import avatar3 from "../images/order.gif"
-import avatar4 from "../images/order.gif"
+import { Link, useNavigate } from "react-router-dom";
+import React from 'react'
+import delivery from "../images/delivery.gif"
+import build from "../images/build.gif"
+import { MDBCard } from 'mdb-react-ui-kit'
 
-
- const Dashborad = () => {
-
-
-    const navigate = useNavigate();
-
+const Dashborad = () => {
+  const navigate = useNavigate();
   const handleSubmit = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("userRole");
     navigate("/Login");
-}
-
-
-
+  }
 
   return (
-	
-    <div className="">
-    <div style={{ textAlign: "center" }}>
-<br></br>
-<br></br>
-   <h1>Welcome to the Site Manager Home </h1>
 
-		<br/>
-
-<div className="container" style={{marginLeft:'20%'}}>
-  <div style={{marginTop:"20px"}}>
-<div class="row row-cols-1 row-cols-md-3">
-  <div class="col mb-4">
-    <div class="card">
-    <img src={avatar} alt="" width={"70%"} height={"60%"} />
-      <div class="card-body">
-        <h5 class="card-title">Request Orders</h5>
-        <p class="card-text">You can view All Suppliers And Request orders.</p>
-        <Link  to="/RequestOrder">
-        <button  className="btn btn-outline-success" type="submit" >
-                    Click Here
-                </button>     
-                </Link>
+    <div className="container shadow my-5 py-5 w-50 hform mx-auto">
+      <h3 className=" fw-bolder"><center><b>Welcome to the Site Manager Home </b></center></h3>
+      <hr /> <br />
+      <div className="container">
+        <div>
+          <div class="row">
+            <div class="col mb-6">
+              <MDBCard shadow='15' border='dark' background='white' className='mb-6'>
+                <h5 class="card-title py-2"><center><b>Request Orders</b></center></h5>
+                <img src={build} alt="build" className="dash_img" />
+                <div class="card-body">
+                  <center>
+                    <p class="card-text">View All Suppliers and Request Orders</p>
+                    <Link to="/RequestOrder">
+                      <button className="btn btn-outline-success" type="submit" >
+                        Click Here
+                      </button>
+                    </Link>
+                  </center>
+                </div>
+              </MDBCard>
+            </div>
+            <div class="col mb-6">
+              <MDBCard shadow='15' border='dark' background='white' className='mb-3'>
+                <h5 class="card-title py-2"> <center><b> All Delivery </b></center></h5>
+                <img src={delivery} alt="delivery" className="dash_img hi" />
+                <div class="card-body">
+                  <center>
+                    <p class="card-text">View all Supplier Delivered Status</p>
+                    <Link to="/DisplayOrderDeliverdStatus">
+                      <button className="btn btn-outline-success mx-auto" type="submit" >
+                        Click Here
+                      </button>
+                    </Link>
+                  </center>
+                </div>
+              </MDBCard>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="col mb-4">
-    <div class="card" style={{marginTop:'20%'}}>
-    <img src={avatar2} alt="" width={"100%"} height={""} />
-      <div class="card-body">
-        <br></br>
-        <h5 class="card-title"> View All Delivery  Status</h5>
-        <p class="card-text">view all supplier delivered status  .</p>
-        <Link  to="/DisplayOrderDeliverdStatus">
-        <button  className="btn btn-outline-success" type="submit" >
-                    Click Here
-                </button>     
-                </Link>   
-         
-      </div>
-      
-    </div>
-  </div>
-
-
-</div>
-</div>
-</div>
-</div>
-</div>
   )
 
 
---
+++ client/src/components/Suuplier/DashboradSupply.js
@@ -0,0 +1,65 @@
+import { Link, useNavigate } from "react-router-dom";
+import React from 'react'
+import delivery from "../images/delivery.gif"
+import build from "../images/build.gif"
+import { MDBCard } from 'mdb-react-ui-kit'
+
+const DashboardSupply = () => {
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userRole");
+    navigate("/Login");
+  }
+
+  return (
+
+    <div className="container shadow my-5 py-5 w-50 hform mx-auto">
+      <h3 className=" fw-bolder"><center><b>Welcome to the Supplier Home </b></center></h3>
+      <hr /> <br />
+      <div className="container">
+        <div>
+          <div class="row">
+            <div class="col mb-6">
+              <MDBCard shadow='15' border='dark' background='white' className='mb-6'>
+                <h5 class="card-title py-2"><center><b>Request Orders</b></center></h5>
+                <img src={build} alt="build" className="dash_img" />
+                <div class="card-body">
+                  <center>
+                    <p class="card-text">View All Suppliers and Request Orders</p>
+                    <Link to="/RequestOrder">
+                      <button className="btn btn-outline-success" type="submit" >
+                        Click Here
+                      </button>
+                    </Link>
+                  </center>
+                </div>
+              </MDBCard>
+            </div>
+            <div class="col mb-6">
+              <MDBCard shadow='15' border='dark' background='white' className='mb-3'>
+                <h5 class="card-title py-2"> <center><b> All Delivery  Status</b></center></h5>
+                <img src={delivery} alt="delivery" className="dash_img hi" />
+                <div class="card-body">
+                  <center>
+                    <p class="card-text">View all Supplier Delivered Status</p>
+                    <Link to="/DisplayOrderDeliverdStatus">
+                      <button className="btn btn-outline-success mx-auto" type="submit" >
+                        Click Here
+                      </button>
+                    </Link>
+                  </center>
+                </div>
+              </MDBCard>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+
+}
+
+
+export default DashboardSupply;
